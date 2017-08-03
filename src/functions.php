5596f79d3bcb41beca4bd767846102128f8f9745
Damian Dziaduch <damian@dziaduch.pl>
2017-08-03 17:43:35 +0200

    Core/Array/array_chunk #7

--- src/functions.php
+++ src/functions.php
@@ -3,4 +3,5 @@
 require_once __DIR__.'/html_entities.php';
 require_once __DIR__.'/class_exists.php';
 require_once __DIR__.'/parse_url.php';
+require_once __DIR__.'/array_chunk.php';
 require_once __DIR__.'/in_array.php';
