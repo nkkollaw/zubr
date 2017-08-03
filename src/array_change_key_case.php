ae57022696be432f3df970ddc289d27c5738e143
Damian Dziaduch <damian@dziaduch.pl>
2017-08-03 17:19:43 +0200

    Core/Array/array_change_key_case #6 - added function

---
+++ src/array_change_key_case.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+
+namespace PHP;
+
+/**
+ * Changes all keys in an array
+ * @link http://php.net/manual/en/function.array-change-key-case.php
+ * @param array $input <p>
+ * The array to work on
+ * </p>
+ * @param int $case [optional] <p>
+ * Either CASE_UPPER or
+ * CASE_LOWER (default)
+ * </p>
+ * @return array an array with its keys lower or uppercased, or false if
+ * input is not an array.
+ * @since 4.2.0
+ * @since 5.0
+ */
+function array_change_key_case(array $input, $case = null)
+{
+    return \array_change_key_case($input, $case);
+}
