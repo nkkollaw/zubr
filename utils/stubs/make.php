61c855bfe24790828b840634e53e0ee4b04971d7
Niccolò Brogi <me@nbrogi.com>
2017-08-15 19:00:53 +0200

    Implement creating actual files

--- utils/stubs/make.php
+++ utils/stubs/make.php
@@ -4,34 +4,98 @@ require_once realpath(__DIR__ . '/../../src/') . '/in_array.php';
 
 use Symfony\Component\Yaml\Yaml;
 use Symfony\Component\Yaml\Exception\ParseException;
+use nkkollaw\Utils\Strings\Strings as StrUtils;
+
+define('PHPSTORM_DIR', __DIR__ . '/phpstorm-stubs/standard');
+define('PHPSTORM_IGNORE', '_standard_manual.php, _types.php, standard_defines.php');
+define('PHPSTORM_FUNC_REGEX', '/(function .* })/U'); // functions declarations
 
 define('LOCUTUS_DIR', __DIR__ . '/locutus/website/source/php'); // contains website files built with `npm run injectweb`
-define('LOCUTUS_COMMENT_PATTERN', '/---(.*?)---/s'); // delimitates comment block
+define('LOCUTUS_IGNORE', '_helpers, index.html');
+define('LOCUTUS_COMMENT_REGEX', '/---(.*?)---/s'); // delimitates comment block
 
-$ignore = [// folders or files to ignore
-    '_helpers',
-    'index.html'
-];
-
-$functions = [];
-
-if (!is_dir(LOCUTUS_DIR) || !is_readable(LOCUTUS_DIR)) {
-    throw new Exception('did you build Locutus..? See README');
-}
-
-foreach (glob(LOCUTUS_DIR . '/*') as $path) {
-    if (\Zubr\in_array($ignore, basename($path))) {
-        continue;
+try {
+    // checks
+    if (!is_dir(PHPSTORM_DIR) || !is_readable(PHPSTORM_DIR)) {
+        throw new Exception('PHPStorm missing');
+    }
+    if (!is_dir(LOCUTUS_DIR) || !is_readable(LOCUTUS_DIR)) {
+        throw new Exception('did you build Locutus..? See README');
     }
 
-    $files = glob($path . '/*');
+    // Build index of functions from tests
+    $functions = [];
+    $ignore = array_map(function($elem) {
+        return trim($elem);
+    }, explode(',', LOCUTUS_IGNORE));
 
-    echo "\n\n---\n";
+    foreach (glob(LOCUTUS_DIR . '/*') as $path) {
+        if (\Zubr\in_array($ignore, basename($path))) {
+            continue;
+        }
 
-    foreach ($files as $file) {
+        $files = glob($path . '/*');
+
+        echo "\n\n---\n";
+
+        foreach ($files as $file) {
+            try {
+                echo "\n Processing $file...";
+
+                if (\Zubr\in_array($ignore, basename($file))) {
+                    echo "\n > Skip.";
+
+                    continue;
+                }
+
+                $file_contents = file_get_contents($file);
+                $matches = [];
+                preg_match(LOCUTUS_COMMENT_REGEX, $file_contents, $matches);
+                $comment_block = $matches[1];
+
+                try {
+                    $comment_block = Yaml::parse($comment_block);
+                } catch (ParseException $e) {
+                    printf("\n > ERROR! Unable to parse the YAML string: %s", $e->getMessage());
+
+                    continue;
+                }
+
+                if (!isset($comment_block['function']) || !$comment_block['function']) {
+                    throw new Exception('no function name');
+                }
+
+                $function_info = [
+                    'name' => $comment_block['function'],
+                    'category' => $comment_block['category'],
+                    'link' => @(new IjorTengab\parseHTML($comment_block['description']))->find('a')->attr('href'),
+                    'tests' => []
+                ];
+                for ($i=0; $i<count($comment_block['examples']); $i++) {
+                    $function_info['tests'][$i] = [
+                        'code' => $comment_block['examples'][$i],
+                        'returns' => $comment_block['returns'][$i]
+                    ];
+                }
+
+                $functions[$function_info['name']] = $function_info;
+            } catch (Exception $e) {
+                echo "\n > ERROR: " . $e->getMessage();
+
+                continue;
+            }
+        }
+    }
+
+    // index PHPStorm stubs
+    $stubs = [];
+    $ignore = array_map(function($elem) {
+        return trim($elem);
+    }, explode(',', PHPSTORM_IGNORE));
+
+    foreach (glob(PHPSTORM_DIR . '/*.php') as $file) {
         try {
-            $file_name = basename($file);
-            echo "\n Processing $path/$file_name...";
+            echo "\n Processing $file...";
 
             if (\Zubr\in_array($ignore, basename($file))) {
                 echo "\n > Skip.";
@@ -41,35 +105,107 @@ foreach (glob(LOCUTUS_DIR . '/*') as $path) {
 
             $file_contents = file_get_contents($file);
             $matches = [];
-            preg_match(LOCUTUS_COMMENT_PATTERN, $file_contents, $matches);
-            $comment_block = $matches[1];
+            preg_match_all(PHPSTORM_FUNC_REGEX, $file_contents, $matches);
 
-            try {
-                $comment_block = Yaml::parse($comment_block);
-            } catch (ParseException $e) {
-                printf("\n > ERROR! Unable to parse the YAML string: %s", $e->getMessage());
+            foreach ($matches[1] as $function_body) {
+                $m = array();
+                preg_match('/function (.*)\(/', $function_body, $m);
+                $function_name = trim($m[1]);
 
-                continue;
+                $stubs[$function_name] = $function_body;
             }
-
-            if (!isset($comment_block['function']) || !$comment_block['function']) {
-                throw new Exception('no function name');
-            }
-
-            $function_info = [
-                'name' => $comment_block['function'],
-                'category' => $comment_block['category'],
-                'link' => @(new IjorTengab\parseHTML($comment_block['description']))->find('a')->attr('href'),
-                'tests' => []
-            ];
-
-            $functions[$function_info['name']] = $function_info;
         } catch (Exception $e) {
             echo "\n > ERROR: " . $e->getMessage();
 
             continue;
         }
     }
-}
 
-var_dump($functions);
+    // write files
+    if (!is_dir(__DIR__ . '/dist/')) {
+        $ok = mkdir(__DIR__ . '/dist/');
+        if (!$ok) {
+            throw new Exception('unable to created "dist" dir');
+        }
+    }
+    if (!is_dir(__DIR__ . '/dist/src/')) {
+        $ok = mkdir(__DIR__ . '/dist/src/');
+        if (!$ok) {
+            throw new Exception('unable to created "src" dir');
+        }
+    }
+    if (!is_dir(__DIR__ . '/dist/tests/')) {
+        $ok = mkdir(__DIR__ . '/dist/tests/');
+        if (!$ok) {
+            throw new Exception('unable to created "tests" dir');
+        }
+    }
+
+    foreach ($stubs as $function_name => $function_body) {
+        if (!isset($functions[$function_name])) {
+            // throw new Exception($function_name . ' was missing from the function index.');
+
+            continue;
+        }
+
+        // create stub file
+        $file_path = "/dist/src/$function_name.php";
+        $function_body = str_replace(' { }', "\n{\n    // TODO: implement\n}", $function_body);
+        $file_contents = '';
+        $file_contents .= <<<EOF
+<?php
+declare(strict_types=1);
+
+/**
+ * \Zubr\\$function_name()
+ *
+ * @link https://secure.php.net/$function_name
+ *
+ * NOTES: same as \\$function_name() || in Zubr, \$param_1 goes before \$param_2, and we return an array instead of void
+*/
+$function_body
+EOF;
+        file_put_contents(__DIR__ . $file_path, $file_contents);
+
+        // create tests file
+        $class_name = ucwords(StrUtils::toCamelCase($function_name));
+        $file_path = '/dist/tests/' . $class_name . '.php';
+        $file_contents = '';
+        $file_contents .= <<<EOF
+<?php
+declare(strict_types=1);
+
+/* These are tests from Locutus for porting
+TODO: implement adding automatically (if it's possible)
+\n
+EOF;
+        foreach ($functions[$function_name]['tests'] as $test_data) {
+            $file_contents .= print_r($test_data, true);
+        }
+        $file_contents .= <<<EOF
+*/
+class $class_name extends PHPUnit\Framework\TestCase
+{
+    public function test()
+    {
+        \$input = ['test1', 'tEsT2', 'TEST3'];
+        \$actual = \\Zubr\\$function_name(/* put stuff here */);
+        \$expected = \\$function_name(/* put stuff here */);
+
+        \$this->assertEquals(\$expected, \$actual);
+    }
+}
+EOF;
+        file_put_contents(__DIR__ . $file_path, $file_contents);
+    }
+
+    echo "\n\n👍🏻  done\n\n";
+} catch (Exception $e) {
+    echo "\n💀\n\n";
+
+    echo 'ERROR: ' . $e->getMessage();
+
+    echo "\n";
+
+    exit(0);
+}
