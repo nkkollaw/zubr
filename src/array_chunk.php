d1ffef27e270fda47710b8ce89b6654671e6f409
Niccolò Brogi <me@nbrogi.com>
2017-09-14 00:20:53 +0200

    Update doc block for array_chunk. Fixes #125.

--- src/array_chunk.php
+++ src/array_chunk.php
@@ -1,25 +1,15 @@
-<?php declare(strict_types=1);
+<?php
+declare(strict_types=1);
 
 namespace Zubr;
 
 /**
- * Split an array into chunks
- * @link http://php.net/manual/en/function.array-chunk.php
- * @param array $input <p>
- * The array to work on
- * </p>
- * @param int $size <p>
- * The size of each chunk
- * </p>
- * @param bool $preserve_keys [optional] <p>
- * When set to true keys will be preserved.
- * Default is false which will reindex the chunk numerically
- * </p>
- * @return array a multidimensional numerically indexed array, starting with zero,
- * with each dimension containing size elements.
- * @since 4.2.0
- * @since 5.0
- */
+ * \Zubr\array_chunk()
+ *
+ * @link https://secure.php.net/array_chunk
+ *
+ * NOTES: same as \array_chunk()
+*/
 function array_chunk(array $input, int $size, bool $preserve_keys = false)
 {
     return \array_chunk($input, $size, $preserve_keys);
