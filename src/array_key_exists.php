3a0cf566ec735f7a3dbf9f9e524e91ad693442eb
Niccolò Brogi <me@nbrogi.com>
2017-09-13 23:15:52 +0200

    Add array_key_exists. Fixes #28.

---
+++ src/array_key_exists.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace Zubr;
+
+/**
+ * \Zubr\array_key_exists()
+ *
+ * @link https://secure.php.net/array_key_exists
+ *
+ * NOTES: same as \array_key_exists()
+*/
+function array_key_exists($key, $search)
+{
+    return \array_key_exists($key, $search);
+}
