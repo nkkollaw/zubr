3d452902d863d3bf39134e52fb276984d3d0f946
Niccolò Brogi <me@nbrogi.com>
2017-09-14 00:00:45 +0200

    Add array_keys. Fixes #29.

---
+++ src/array_keys.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace Zubr;
+
+/**
+ * \Zubr\array_keys()
+ *
+ * @link https://secure.php.net/array_keys
+ *
+ * NOTES: same as \array_keys()
+*/
+function array_keys(array $input, $search_value = null, $strict = false)
+{
+    return \array_keys($input, $search_value, $strict);
+}
