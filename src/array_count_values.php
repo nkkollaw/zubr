4ef7920ee2e31edce26ddaf44d50da8601993f95
Niccolò Brogi <me@nbrogi.com>
2017-09-13 22:27:02 +0200

    Add array_count_values (fix #10)

---
+++ src/array_count_values.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace Zubr;
+
+/**
+ * \Zubr\array_count_values()
+ *
+ * @link https://secure.php.net/array_count_values
+ *
+ * NOTES: same as \array_count_values()
+*/
+function array_count_values($input)
+{
+    return \array_count_values($input);
+}
